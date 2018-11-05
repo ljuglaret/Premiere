cb1274839f6aed6b6cedcc51a043dc71efd47bd8
Laure Juglaret <ljuglaret@gmail.com>
2018-11-05 20:26:35 +0000

    Initial commit

---
+++ pm.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+public class pm{
+    public static void fct(int nombre){
+        int coups = 0 ;
+        Scanner sc = new Scanner (System.in);
+        Random r = new Random();
+        int  saisie = r.nextInt(50) + 1;
+
+    /*
+    Demander la saisie d un entier;
+    Si l entier est correct afficher un message indiquant que la partie
+    est finie et le score.
+    Sinon afficher une indication ("le nombre doit etre superieur/inferieur")
+    */
+
+    }
+ 
+    public static void main (String[] args) {
+        fct(x);
+        /*
+        Pour le moment c est correct mais l utilisateur ne peut effectuer qu une seule partie,
+        modifier le code pour qu il puisse jouer autant de parties qu il le souhaite.
+        Par exemple vous pouvez 
+            - declarer une chaine de caractere s , l initialiser a "Oui" 
+            - tant que la chaine vaut Y
+                - lancer une partie
+                - afficher "Souhaitez vous jouer de nouveau : Oui ou Non",
+        */
+
+    }
+}
